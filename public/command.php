7b49bb3c9705721a407aa6642776b6ee5b23a617
rodrigobutta <rbutta@gmail.com>
2019-11-30 19:36:07 -0300

    command dispatcher clean up

--- public/command.php
+++ public/command.php
@@ -2,12 +2,7 @@
 require_once __DIR__.'/../bootstrap.php';
 require_once __DIR__.'/../routes/command.php';
 
-if (isset($argc)) {
-    echo $argv[1] . "\n";
-    echo $argv[2] . "\n";
-    echo $argv[3] . "\n";
-}
-else {
+if (!isset($argc)) {
     echo "ERROR: argc and argv disabled\n";
     exit();
 }
--- routes/command.php
+++ routes/command.php
@@ -34,21 +34,4 @@ function dispatch($args){
             break;
     }
 
-    
 }
-
-// $routing->with('/shout', function () use ($routing, $datasources) {
-
-//     $controller = new ShoutController($datasources,'database');
-
-//     $routing->respond('GET', '/?', function ($request, $response) {
-//         return 'You might be forgeting the author..';
-//     });
-
-//     $routing->respond('GET', '/[:author]', function ($request, $response) use($controller){
-
-//         $controller->getByAuthor($request);
-
-//     });
-
-// });
