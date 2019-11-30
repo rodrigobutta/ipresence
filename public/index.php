c5d11578ca17dacde9a85945ebbe3e36f5603728
rodrigobutta <rbutta@gmail.com>
2019-11-30 10:42:42 -0300

    basic routing for now

--- public/index.php
+++ public/index.php
@@ -8,6 +8,36 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
-echo '[]';
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode('/', $uri);
+
+if (!$uri[1]) {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+
+switch ($uri[1]) {
+    case 'shout':
+        
+        $author = null;
+        if (isset($uri[2])) {
+            $author = $uri[2];
+        }
+
+        echo 'route ok';
+
+
+        break;
+    
+    default:
+        header("HTTP/1.1 404 Not Found");
+        exit();
+        break;
+}
+
+
+
+
 
 ?>
